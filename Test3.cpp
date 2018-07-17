b08f0478d4187e06b6de777dba4bcb6244f9767d
Ludovic Theobald <ludovictheobald@Ludovics-MacBook-Pro.local>
2018-07-17 14:29:17 +0700

    17/07

---
+++ Test3.cpp
@@ -0,0 +1,8 @@
+//
+//  Test3.cpp
+//  
+//
+//  Created by Ludovic Theobald on 9/7/2561 BE.
+//
+
+#include "Test3.hpp"
---
+++ Test3.hpp
@@ -0,0 +1,13 @@
+//
+//  Test3.hpp
+//  
+//
+//  Created by Ludovic Theobald on 9/7/2561 BE.
+//
+
+#ifndef Test3_hpp
+#define Test3_hpp
+
+#include <stdio.h>
+
+#endif /* Test3_hpp */
